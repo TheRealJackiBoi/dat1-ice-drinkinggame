a25776e5b8df12fa867d12e8193e11901b9ef3f3
Julius Lassen <julius5517@gmail.com>
2022-12-12 12:28:19 +0100

    MainActivity + button action added

--- app/src/main/java/com/example/dat1_ice_drinkinggame/MainActivity.java
+++ app/src/main/java/com/example/dat1_ice_drinkinggame/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.dat1_ice_drinkinggame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mainB = (Button) findViewById(R.id.main_button);
+        mainB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity2();
+            }
+        });
+
     }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this, NewGameFragment.class);
+        startActivity(intent);
+    }
+
+
 }
