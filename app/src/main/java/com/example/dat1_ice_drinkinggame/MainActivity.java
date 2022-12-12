08c68c755edec211e87ebac8548a2527d20b51b7
Julius Lassen <julius5517@gmail.com>
2022-12-12 16:53:18 +0100

    HomeFragment + button action Working

--- app/src/main/java/com/example/dat1_ice_drinkinggame/MainActivity.java
+++ app/src/main/java/com/example/dat1_ice_drinkinggame/MainActivity.java
@@ -1,35 +1,42 @@
 package com.example.dat1_ice_drinkinggame;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHost;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.dat1_ice_drinkinggame.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        Button mainB = (Button) findViewById(R.id.main_button);
-        mainB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openActivity2();
-            }
-        });
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+
+
 
     }
-
-    public void openActivity2(){
-        Intent intent = new Intent(this, NewGameFragment.class);
-        startActivity(intent);
-    }
-
 
 }
