5e39ddba74c971d3bbd427eebc29d4efc2265a71
artiecodes <bjarke.dehlie@gmail.com>
2022-12-12 12:47:42 +0100

    NewGameFragment WIP

--- app/src/main/java/com/example/dat1_ice_drinkinggame/Player.java
+++ app/src/main/java/com/example/dat1_ice_drinkinggame/Player.java
@@ -1,4 +1,11 @@
 package com.example.dat1_ice_drinkinggame;
 
 public class Player {
+    int id;
+    String name;
+
+    public Player(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
