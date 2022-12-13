236bfd76ea7be24ea712fd162feda5c73857055a
TheRealJackiBoi <jslam@oulund.dk>
2022-12-13 12:53:26 +0100

    FileIO Implement

--- app/src/main/java/com/example/dat1_ice_drinkinggame/Card.java
+++ app/src/main/java/com/example/dat1_ice_drinkinggame/Card.java
@@ -2,15 +2,11 @@ package com.example.dat1_ice_drinkinggame;
 
 public class Card {
 
-    private final String name;
     private final String challenge;
     private final int sips;
 
-    public Card(String name, String challenge, int sips) {
-        this.name = name;
+    public Card(String challenge, int sips) {
         this.challenge = challenge;
         this.sips = sips;
     }
-
-
 }
